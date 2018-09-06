2a87dbafbafc3b8c2aadb5ebff920628fdbb4877
marcuscaum <marcuslucas.jb@gmail.com>
2018-09-06 12:12:19 -0300

    Setup tests and add a few assets

---
+++ src/pages/SignUpPage/index.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import SignUpPage from './index';
+
+const setup = props => shallow(<SignUpPage {...props} />);
+
+describe('[Page] SignUpPage', () => {
+  it('should render the page', () => {
+    const wrapper = setup({});
+    expect(wrapper).toMatchSnapshot();
+  });
+});
