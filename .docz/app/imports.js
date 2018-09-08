af550872bc8933efa0b177133c6329e241b25ba9
marcuscaum <marcuslucas.jb@gmail.com>
2018-09-08 15:09:37 -0300

    Add FormField documentation

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -3,4 +3,6 @@ export const imports = {
     import(/* webpackPrefetch: true, webpackChunkName: "src-components-button-index" */ 'src/components/Button/index.mdx'),
   'src/components/Checkbox/index.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "src-components-checkbox-index" */ 'src/components/Checkbox/index.mdx'),
+  'src/components/FormField/index.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "src-components-form-field-index" */ 'src/components/FormField/index.mdx'),
 }
